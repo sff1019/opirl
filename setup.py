3795e24201515275974a26133aa7e8feca47f345
sff1019 <starfield1019@gmail.com>
2021-09-10 03:26:01 +0900

    Fix package name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='opirl',
     packages=[
-        package for package in find_packages() if package.startswith('opirl')
+        package for package in find_packages() if package.startswith('src')
     ],
     version='0.1.0',
 )
